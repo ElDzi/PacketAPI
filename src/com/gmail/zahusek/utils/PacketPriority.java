40eee7d7d9abc187bdca21ef0126bc33a9e7dabc
Zahusek <zahusek@gmail.com>
2015-03-21 12:36:38 +0100

    Update PacketPriority.java

--- src/com/gmail/zahusek/utils/PacketPriority.java
+++ src/com/gmail/zahusek/utils/PacketPriority.java
@@ -1,1 +1,15 @@
-...
+package com.gmail.zahusek.utils;
+
+public enum PacketPriority {
+	HIGHEST(0), HIGH(1), NORMAL(2), LOW(3), LOWEST(4);
+	
+	private final int value;
+   
+	private PacketPriority(int value) {
+        this.value = value;
+    }
+   
+	public int getValue() {
+        return value;
+    }
+}
